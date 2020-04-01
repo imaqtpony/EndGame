51b8ddf5b751fe87ce523d2e120decc3c07715d6
imaqtpony <62941159+imaqtpony@users.noreply.github.com>
2020-04-01 17:02:24 +0200

    initial

---
+++ Assets/Scripts/PlayerRelated/MovePlayer.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+
+// estce que on clic et on attend la fi n du path ?
+public class MovePlayer : MonoBehaviour
+{
+    // Component NavMeshAgent
+    private NavMeshAgent m_agent;
+
+    // range in which it detects a hit on the navmesh, from the touch on screen
+    private float m_range = 10f;
+
+
+
+    private void Awake()
+    {
+        m_agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    private void Update()
+    {
+        Debug.Log(m_agent.hasPath);
+
+        //if (pathStat == "PathComplete" && input get touch 0 ?
+        //{
+        //    AgentPathUpdate();
+        //}
+
+        DetermineHitOnScreen();
+        
+
+    }
+
+
+    /// <summary>
+    /// Raycasts the touch on screen to a point in the world 
+    /// </summary>
+    private void DetermineHitOnScreen()
+    {
+        if (Input.touchCount > 0)
+        {
+            // remove this to revert to follow the finger type of movement
+            if (!m_agent.hasPath)
+            {
+                Touch touch = Input.GetTouch(0);
+                RaycastHit hit;
+
+                // if le tel marche plus
+                //Vector3 mouse = Input.mousePosition;
+                //Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+                Ray castPoint = Camera.main.ScreenPointToRay(touch.position);
+
+                if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+                {
+                    m_agent.destination = ClosestNavmeshLocation(hit.point, m_range);
+
+                }
+            }
+
+        }
+    }
+
+
+    /// <summary>
+    /// Generate the closest hit on navmesh from the player touch on screen when moving
+    /// possible adds : Set this + delegate if effect or feedback when changing var ?  befare of range tho
+    /// </summary>
+    private Vector3 ClosestNavmeshLocation(Vector3 p_v, float p_range)
+    {
+        
+        NavMeshHit hit;
+
+        Vector3 closestPosition = Vector3.zero;
+
+        // Get the closest navmeshhit
+        if (NavMesh.SamplePosition(p_v, out hit, p_range, 1))
+        {
+            closestPosition = hit.position;
+        }
+
+
+        return closestPosition;
+
+    }
+
+}
