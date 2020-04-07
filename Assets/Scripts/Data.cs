4be203a797e47e418f42670184a3bb7f11e84ad1
imaqtpony <62941159+imaqtpony@users.noreply.github.com>
2020-04-07 17:54:19 +0200

    Ajout d'un SO de data

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Data", order = 1)]
+public class Data : ScriptableObject
+{
+
+    // les vars en public
+
+
+
+}
