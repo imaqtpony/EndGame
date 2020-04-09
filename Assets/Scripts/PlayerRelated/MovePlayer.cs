0afeb5b68f8835b08a06eb708e5c39e8d5404814
imaqtpony <62941159+imaqtpony@users.noreply.github.com>
2020-04-09 19:20:06 +0200

    ajout push object

--- Assets/Scripts/PlayerRelated/MovePlayer.cs
+++ Assets/Scripts/PlayerRelated/MovePlayer.cs
@@ -14,10 +14,13 @@ public class MovePlayer : MonoBehaviour
     // range in which it detects a hit on the navmesh, from the touch on screen
     private float m_range = 10f;
 
+    private Rigidbody m_rb;
+
 
 
     private void Awake()
     {
+        m_rb = GetComponent<Rigidbody>();
         m_agent = GetComponent<NavMeshAgent>();
     }
 
@@ -26,30 +29,49 @@ public class MovePlayer : MonoBehaviour
     {
         Debug.Log(m_agent.hasPath);
 
+        //string pathStat = m_agent.pathStatus.ToString();
+        //Debug.Log("z axis " + transform.forward.z);
+        //Debug.Log("x axis " + transform.forward.x);
         //if (pathStat == "PathComplete" && input get touch 0 ?
         //{
         //    AgentPathUpdate();
         //}
 
+        //nah
+        //if (m_agent.hasPath && m_rb.velocity.magnitude < 0.05f)
+        //{
+        //    m_agent.isStopped = true;
+        //    m_agent.ResetPath();
+        //}
 
-        if (Input.touchCount > 0 )
-        {
+        // the pathcomplete bug fix where the agent stops but his path is not completed
+        if ((transform.position.x < m_agent.destination.x + 0.2f && transform.position.x > m_agent.destination.x - 0.2f) && (transform.position.z < m_agent.destination.z + 0.2f && transform.position.z > m_agent.destination.z - 0.2f))
+            m_agent.ResetPath();
+
+        //if le tel marche
+        //if (Input.touchCount > 0 )
+        //{
+        if (Input.GetMouseButtonDown(0)) {
             // remove this to revert to follow the finger type of movement
+
             if (!m_agent.hasPath)
             {
-                Touch touch = Input.GetTouch(0);
+                //if le tel marche
+                //Touch touch = Input.GetTouch(0);
+                //Ray castPoint = Camera.main.ScreenPointToRay(touch.position);
                 RaycastHit hit;
 
-                // if le tel marche plus
-                //Vector3 mouse = Input.mousePosition;
-                //Ray castPoint = Camera.main.ScreenPointToRay(mouse);
-                Ray castPoint = Camera.main.ScreenPointToRay(touch.position);
+                //if le tel marche plus
+                Vector3 mouse = Input.mousePosition;
+                Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+
 
                 if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
                 {
                     m_agent.destination = ClosestNavmeshLocation(hit.point, m_range);
 
                 }
+                Debug.Log(m_agent.destination);
             }
             
         }
---
+++ Assets/Scripts/Pushable_Object.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pushable_Object : MonoBehaviour
+{
+
+    private bool m_pushThisOne = false;
+    private bool m_playerWithinRange = false;
+    private Rigidbody m_rb;
+
+    //private Transform m_playerTransform;
+
+    // create a GO to simulate better the directional vector for the force to apply rather than the players forward (cause he takes time to rotate)
+    private GameObject m_forceDirection;
+
+
+    private void Awake()
+    {
+        m_forceDirection = new GameObject();
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+
+        Vector3 mouse = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        RaycastHit hit;
+
+
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+        {
+            // hit on the object
+            m_pushThisOne = true;
+
+        }
+
+        //if hit est pas nul
+        // normale de player a hit
+        if (m_pushThisOne && m_playerWithinRange)
+        {
+            //maybe instead get a forward from the player pos to the mouse pos
+            m_rb.AddForce(new Vector3(m_forceDirection.transform.forward.x,0, m_forceDirection.transform.forward.z) * 5f, ForceMode.Impulse);
+            Debug.Log("Successfully pushed obj");
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            m_playerWithinRange = true;
+
+
+        m_forceDirection.transform.position = other.gameObject.transform.position;
+        m_forceDirection.transform.rotation = other.gameObject.transform.rotation;
+        // push to center of the object
+        m_forceDirection.transform.LookAt(transform);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            m_playerWithinRange = false;
+    }
+
+
+}
