2671927e48fc942bda740fcad62858eb8cbe2dc0
imaqtpony <62941159+imaqtpony@users.noreply.github.com>
2020-04-28 10:18:43 +0200

    ghgf

--- Assets/Scripts/AI/Spawn.cs
+++ Assets/Scripts/AI/Spawn.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    private GameObject m_prefab;   
+    [SerializeField] GameObject m_prefab;   
 
     void Awake()
     {
-
-        m_prefab = GameObject.Find("Ennemy");
         Instantiate(m_prefab, transform.position, transform.rotation);
     }
 }
