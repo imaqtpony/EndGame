baeda19cc22032613439e27454c6925048885864
Sadjjay <61274892+Sadjjay@users.noreply.github.com>
2020-05-07 16:44:54 +0200

    Drop de l'item d'un objet coupable

--- Assets/Scripts/InteractiveObjects/EnvironementObject.cs
+++ Assets/Scripts/InteractiveObjects/EnvironementObject.cs
@@ -1,16 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using InventoryNS.Utils;
 
 public class EnvironementObject : MonoBehaviour
 {
-    //ce script pour generer les items quand l'objet du world creve, et s'autodetruire
-    protected void DropMaterialOnDeath(Item p_item, float p_destroyTime)
-    {
 
-        //Item item = GenerateItem(transform.position, p_item.ItemType);
-        Destroy(gameObject, p_destroyTime);
-        Debug.Log("DETRUIS TOIIII");
+    public Vector3 GetPosition()
+    {
+        return transform.position;
+    }
+
+    //ce script pour generer les items quand l'objet du world creve, et s'autodetruire
+    protected void DropMaterialOnDeath(bool p_isBurned, float p_destroyTime, int p_amountItem, Item.ItemType p_itemType)
+    {
+        if (!p_isBurned)
+        {
+            //Item item = GenerateItem(transform.position, p_item.ItemType);
+            Destroy(gameObject, p_destroyTime);
+            Debug.Log("DETRUIS TOIIII");
+
+            Item duplicateItem = new Item { itemType = p_itemType, amount = p_amountItem };
+            ItemWorld.DropItem(gameObject.transform.position, duplicateItem);
+        }
+        else
+        {
+            Destroy(gameObject, p_destroyTime);
+
+        }
+
 
     }
 
